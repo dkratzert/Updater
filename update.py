c8881899bfa2996e7ec1f56301d31d5c6b06b6c0
Daniel Kratzert <dkratzert@gmx.de>
2020-12-03 09:18:28 +0100

    support multiple download urls

--- update.py
+++ update.py
@@ -12,7 +12,7 @@ from pathlib import Path
 from urllib import request
 from urllib.error import HTTPError
 
-import progressbar as progressbar
+from progressbar import ProgressBar
 
 """
 return codes:
@@ -22,16 +22,17 @@ return codes:
 4: No command line options supplied
 """
 
-urls = {
-    'dsr': 'https://xs3-data.uni-freiburg.de/data/DSR-setup-{}.exe',
-    'structurefinder': 'https://xs3-data.uni-freiburg.de/structurefinder/StructureFinder-setup-x64-v{}.exe',
-    'finalcif': 'https://xs3-data.uni-freiburg.de/finalcif/FinalCif-setup-x64-v{}.exe',
-    'test': 'https://xs3-data.uni-freiburg.de/test/test-v{}.exe',
+download_urls = {
+    'dsr': ['https://xs3-data.uni-freiburg.de/data/DSR-setup-{}.exe'],
+    'structurefinder': ['https://xs3-data.uni-freiburg.de/structurefinder/StructureFinder-setup-x64-v{}.exe'],
+    'finalcif': ['https://xs3-data.uni-freiburg.de/finalcif/FinalCif-setup-x64-v{}.exe',
+                 ''],
+    'test': ['https://xs3-data.uni-freiburg.de/test/test-v{}.exe'],
 }
 
 
 def show_help():
-    print('############ Program updater V2 #################')
+    print('############ Program updater V3 #################')
     print('Command line options:')
     print('-v version  : Version number of the installer executable')
     print('-p name     : Program name')
@@ -49,7 +50,7 @@ def get_option(option: str = '') -> str:
 
 
 def show_progress(block_num, block_size, total_size):
-    pbar = progressbar.ProgressBar(maxval=total_size)
+    pbar = ProgressBar(maxval=total_size)
     pbar.start()
     downloaded = block_num * block_size
     if downloaded < total_size:
@@ -61,58 +62,93 @@ def show_progress(block_num, block_size, total_size):
 def fetch_update() -> None:
     if not any_options_supplied():
         show_help()
-        os.system('pause')
+        if platform_is("linux"):
+            pass
+        elif platform_is("darwin"):
+            pass
+        elif platform_is("win"):
+            os.system('pause')
         sys.exit(4)
     version = get_option('-v')
     program_name = get_option('-p')
-    url = urls.get(program_name)
-    if not url:
+    urls = download_urls.get(program_name)
+    if not urls:
         print('Unknown program. Aborting update!')
         return None
     # This is the program setup that installs the new version:
     program_path = Path('{}-setup.exe'.format(program_name))
     kill_program_instances(program_name)
-    if finalcif_is_still_running():
+    if programm_is_still_running(program_name):
         sys.exit(3)
     tmp_dir = Path(__file__).parent
-    try:
-        print('Downloading setup file from:', url.format(version))
-        file_name, header = request.urlretrieve(url=url.format(version), filename=tmp_dir.joinpath(program_path),
-                                                reporthook=show_progress)
-    except HTTPError as e:
-        check_for_404_error(e)
-        return None
-    print('Finished.')
-    if is_checksum_valid(file_name, url.format(version)):
-        print('Checksum OK')
-        run_updater(file_name)
-    else:
-        print('Checksum Failed. Aborting update!')
-        subprocess.run(["pause"], shell=True)
-        return None
+    for url in urls:
+        file_name = try_download(program_path, tmp_dir, url, version)
+        checksum_state = check_checksum(file_name, url, version)
+        if checksum_state:
+            if run_updater(file_name):
+                print('Finished successfully.')
+                return
+        else:
+            print('Checksum Failed. Update with this file not possible.')
+    print('\nGiving up.')
+    if platform_is("linux"):
+        pass
+    elif platform_is("darwin"):
+        pass
+    elif platform_is("win"):
+        os.system("pause")
     return None
 
 
-def is_checksum_valid(setupfile, sha_url):
-    # download SHA file:
-    sha_url = str(sha_url)[:-4] + '-sha512.sha'
-    print('\nDownloading sha file from:', sha_url)
-    try:
-        shafile, header = request.urlretrieve(url=sha_url, reporthook=show_progress)
-    except HTTPError as e:
-        check_for_404_error(e)
-        return False
-    # Checksum for program package:
-    sha = sha512_checksum(setupfile)
-    if sha == Path(shafile).read_text():
+def check_checksum(file_name: str, url: str, version: str):
+    if is_checksum_valid(file_name, url.format(version)):
+        print('Checksum OK')
         return True
     else:
         return False
 
 
-def check_for_404_error(e):
-    if e.code == 404:
-        print('Could not download file!')
+def platform_is(plat: str) -> bool:
+    return sys.platform.startswith(plat)
+
+
+def try_download(program_path: str, tmp_dir: Path, url: str, version: str):
+    try:
+        print('Downloading setup file from:', url.format(version))
+        file_name, header = request.urlretrieve(url=url.format(version),
+                                                filename=tmp_dir.joinpath(program_path),
+                                                reporthook=show_progress)
+    except (HTTPError, ValueError):
+        print('Failed to download, trying different URL...')
+        return None
+    if file_name:
+        print('Download succeeded.')
+        return file_name
+
+
+def is_checksum_valid(setupfile: str, sha_url: str) -> bool:
+    # download SHA file:
+    shafile = ''
+    sha_url = str(sha_url)[:-4] + '-sha512.sha'
+    print('\nDownloading sha file from:', sha_url)
+    try:
+        shafile, header = request.urlretrieve(url=sha_url, reporthook=show_progress)
+    except (HTTPError, ValueError):
+        print('Failed to download, trying different URL...')
+    if shafile:
+        # Checksum for program package:
+        sha_from_setup_file = sha512_checksum(setupfile)
+        sha_from_sha_file = Path(shafile).read_text()
+        return checksums_match(sha_from_setup_file, sha_from_sha_file)
+    else:
+        return False
+
+
+def checksums_match(sha_of_setup: str, sha_of_sha_file: str):
+    if sha_of_setup == sha_of_sha_file:
+        return True
+    else:
+        return False
 
 
 def sha512_checksum(filename, block_size=65536):
@@ -127,12 +163,22 @@ def sha512_checksum(filename, block_size=65536):
 
 
 def kill_program_instances(program_name: str):
-    subprocess.call(["taskkill", "/f", "/im", "{}.exe".format(program_name)], stdout=0, stderr=0, shell=True)
-    subprocess.call(["cls"], shell=True)
+    if platform_is("linux"):
+        pass
+    elif platform_is("darwin"):
+        pass
+    elif platform_is("win"):
+        subprocess.call(["taskkill", "/f", "/im", "{}.exe".format(program_name)], stdout=0, stderr=0, shell=True)
+        subprocess.call(["cls"], shell=True)
 
 
-def finalcif_is_still_running():
-    return [x for x in os.popen('tasklist').readlines() if 'finalcif' in x.lower()]
+def programm_is_still_running(program: str) -> bool:
+    if platform_is("linux"):
+        return False
+    elif platform_is("darwin"):
+        return False
+    elif platform_is("win"):
+        return any([x for x in os.popen('tasklist').readlines() if program in x.lower()])
 
 
 def any_options_supplied():
@@ -142,12 +188,16 @@ def any_options_supplied():
         return False
 
 
-def run_updater(filename: str):
-    subprocess.Popen([filename])
-
-
-def remove_update_executable(filename: str) -> None:
-    Path(filename).unlink()
+def run_updater(filename: str) -> bool:
+    if platform_is("linux"):
+        print('No update possible in Linux.')
+        return False
+    elif platform_is("darwin"):
+        print('No update possible in MacOS.')
+        return False
+    elif platform_is("win"):
+        subprocess.Popen([filename])
+        return True
 
 
 if __name__ == '__main__':
