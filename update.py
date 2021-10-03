49b6880e10388d15e0cf02c01ef8818f400f64ab
Daniel Kratzert <dkratzert@gmx.de>
2021-10-03 12:41:20 +0200

    improved update behavior and error messages

--- update.py
+++ update.py
@@ -8,6 +8,7 @@ import hashlib
 import os
 import subprocess
 import sys
+from contextlib import suppress
 from pathlib import Path
 from urllib import request
 from urllib.error import HTTPError
@@ -24,25 +25,30 @@ return codes:
 
 download_urls = {
     # The URLs are a list in order to make different locations possible:
-    'dsr'            : ['https://xs3-data.uni-freiburg.de/data/DSR-setup-{}.exe',
-                        'https://github.com/dkratzert/DSR/raw/master/DSR-setup-{}.exe',
-                        'https://dkratzert.de/files/DSR/DSR-setup-{}.exe'],
-    'structurefinder': ['https://xs3-data.uni-freiburg.de/structurefinder/StructureFinder-setup-x64-v{}.exe',
-                        'https://github.com/dkratzert/Structurefinder/raw/master/StructureFinder-setup-x64-v{}.exe',
-                        'https://dkratzert.de/files/structurefinder/StructureFinder-setup-x64-v{}.exe',
-                        ],
-    'finalcif'       : ['https://xs3-data.uni-freiburg.de/finalcif/FinalCif-setup-x64-v{}.exe',
-                        'https://github.com/dkratzert/FinalCif/raw/master/FinalCif-setup-x64-v{}.exe',
-                        'https://dkratzert.de/files/finalcif/FinalCif-setup-x64-v{}.exe',
-                        ],
-    'test'           : ['https://xs3-data.uni-freiburg.de/test/test-v{}.exe',
-                        'https://github.com/dkratzert/FinalCif/raw/master/test-v{}.exe',
-                        'https://dkratzert.de/files/test-v{}.exe',
-                        ],
+    'dsr'            : [
+        'https://dkratzert.de/files/DSR/DSR-setup-{}.exe',
+        'https://xs3-data.uni-freiburg.de/data/DSR-setup-{}.exe',
+        'https://github.com/dkratzert/DSR/raw/master/DSR-setup-{}.exe',
+    ],
+    'structurefinder': [
+        'https://dkratzert.de/files/structurefinder/StructureFinder-setup-x64-v{}.exe',
+        'https://xs3-data.uni-freiburg.de/structurefinder/StructureFinder-setup-x64-v{}.exe',
+        'https://github.com/dkratzert/Structurefinder/raw/master/StructureFinder-setup-x64-v{}.exe',
+    ],
+    'finalcif'       : [
+        'https://dkratzert.de/files/finalcif/FinalCif-setup-x64-v{}.exe',
+        'https://xs3-data.uni-freiburg.de/finalcif/FinalCif-setup-x64-v{}.exe',
+        'https://github.com/dkratzert/FinalCif/raw/master/FinalCif-setup-x64-v{}.exe',
+    ],
+    'test'           : [
+        'https://dkratzert.de/files/test-v{}.exe',
+        'https://xs3-data.uni-freiburg.de/test/test-v{}.exe',
+        'https://github.com/dkratzert/FinalCif/raw/master/test-v{}.exe',
+    ],
 }
 
 
-def show_help():
+def show_help() -> None:
     print('############ Program updater V5 #################')
     print('Command line options:')
     print('-v version  : Version number of the installer executable')
@@ -62,7 +68,7 @@ def get_option(option: str = '') -> str:
     return ''
 
 
-def show_progress(block_num, block_size, total_size):
+def show_progress(block_num, block_size, total_size) -> None:
     pbar = ProgressBar(maxval=total_size)
     pbar.start()
     downloaded = block_num * block_size
@@ -88,33 +94,40 @@ def fetch_update() -> None:
     if programm_is_still_running(program_name):
         sys.exit(3)
     tmp_dir = Path(__file__).parent
-    have_downloaded = perform_download(program_path, tmp_dir, urls, version)
-    if have_downloaded:
+    downloaded_update = perform_download(program_path, tmp_dir, urls, version)
+    if downloaded_update:
+        run_updater(downloaded_update)
         print('Finished successfully.')
     else:
-        print('Checksum Failed. Update with this file not possible.')
         print('\nGiving up.')
         if platform_is("win"):
             os.system("pause")
     return None
 
 
-def perform_download(program_path, tmp_dir, urls, version):
+def perform_download(program_path, tmp_dir, urls, version) -> str:
     for url in urls:
-        file_name = try_download(program_path, tmp_dir, url, version)
+        full_url = url.format(version)
+        print('Downloading setup file from:', full_url)
+        file_name = try_download(program_path, tmp_dir, full_url)
         checksum_state = check_checksum(file_name, url, version)
         if checksum_state:
-            if run_updater(file_name):
-                return True
+            return file_name
         else:
-            return False
+            print('Failed to update, trying different URL...\n')
+    return ''
 
 
-def check_checksum(file_name: str, url: str, version: str):
-    if is_checksum_valid(file_name, url.format(version)):
+def check_checksum(file_name: str, url: str, version: str) -> bool:
+    shafile = download_checksum(sha_url=url.format(version))
+    if not shafile:
+        print('No checksum file found at URL.')
+        return False
+    if is_checksum_valid(file_name, shafile=shafile):
         print('Checksum OK')
         return True
     else:
+        print('Checksum Failed. Update with this file not possible.')
         return False
 
 
@@ -122,29 +135,21 @@ def platform_is(plat: str) -> bool:
     return sys.platform.startswith(plat)
 
 
-def try_download(program_path: Path, tmp_dir: Path, url: str, version: str):
+def try_download(program_path: Path, tmp_dir: Path, full_url: str) -> str:
     try:
-        print('Downloading setup file from:', url.format(version))
-        file_name, header = request.urlretrieve(url=url.format(version),
+        file_name, header = request.urlretrieve(url=full_url,
                                                 filename=tmp_dir.joinpath(program_path),
                                                 reporthook=show_progress)
     except (HTTPError, ValueError):
-        print('Failed to download, trying different URL...')
-        return None
+        print('Download failed.')
+        return ''
     if file_name:
-        print('Download succeeded.')
+        print('-> Download succeeded.')
         return file_name
 
 
-def is_checksum_valid(setupfile: str, sha_url: str) -> bool:
+def is_checksum_valid(setupfile: str, shafile: str) -> bool:
     # download SHA file:
-    shafile = ''
-    sha_url = str(sha_url)[:-4] + '-sha512.sha'
-    print('\nDownloading sha file from:', sha_url)
-    try:
-        shafile, header = request.urlretrieve(url=sha_url, reporthook=show_progress)
-    except (HTTPError, ValueError):
-        print('Failed to download, trying different URL...')
     if shafile:
         # Checksum for program package:
         sha_from_setup_file = sha512_checksum(setupfile)
@@ -154,14 +159,22 @@ def is_checksum_valid(setupfile: str, sha_url: str) -> bool:
         return False
 
 
-def checksums_match(sha_of_setup: str, sha_of_sha_file: str):
+def download_checksum(sha_url):
+    shafile = ''
+    sha_url = str(sha_url)[:-4] + '-sha512.sha'
+    with suppress(HTTPError, ValueError):
+        shafile, header = request.urlretrieve(url=sha_url, reporthook=show_progress)
+    return shafile
+
+
+def checksums_match(sha_of_setup: str, sha_of_sha_file: str) -> bool:
     if sha_of_setup == sha_of_sha_file:
         return True
     else:
         return False
 
 
-def sha512_checksum(filename, block_size=65536):
+def sha512_checksum(filename, block_size=65536) -> str:
     """
     Calculates a SHA512 checksum from a file.
     """
@@ -172,11 +185,11 @@ def sha512_checksum(filename, block_size=65536):
     return sha512.hexdigest()
 
 
-def kill_program_instances(program_name: str):
+def kill_program_instances(program_name: str) -> None:
     if platform_is("linux"):
-        pass
+        print()
     elif platform_is("darwin"):
-        pass
+        print()
     elif platform_is("win"):
         subprocess.call(["taskkill", "/f", "/im", "{}.exe".format(program_name)], stdout=0, stderr=0, shell=True)
         subprocess.call(["cls"], shell=True)
@@ -191,7 +204,7 @@ def programm_is_still_running(program: str) -> bool:
         return any([x for x in os.popen('tasklist').readlines() if program in x.lower()])
 
 
-def any_options_supplied():
+def any_options_supplied() -> bool:
     if len(sys.argv) > 3:
         return True
     else:
