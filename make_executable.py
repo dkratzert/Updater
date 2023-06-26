4d84e352d5fce7f31312211c77dba01a8baffbb1
Daniel Kratzert <dkratzert@gmx.de>
2023-06-26 22:01:07 +0200

    Update make_executable.py

--- make_executable.py
+++ make_executable.py
@@ -1,3 +1,4 @@
 import subprocess
 
 subprocess.call(['venv\Scripts\pyinstaller.exe', '--onefile', 'src/updater/update.py'])
+subprocess.call(['python', '-m', 'nuitka', 'src\updater\update.py', '--onefile'])
