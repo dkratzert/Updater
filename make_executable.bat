7d7b59b820d80c96972983142bf51fcf62e71285
Daniel Kratzert <dkratzert@gmx.de>
2023-06-28 08:22:34 +0200

    add build script

---
+++ make_executable.bat
@@ -0,0 +1,10 @@
+
+call venv\scripts\activate
+
+python.exe -m pip install --upgrade pip
+pip install -r requirements-devel.txt -U
+
+venv\Scripts\pyinstaller.exe --onefile src/updater/update.py
+
+REM  Alternatively, but virus scanners do not like it:
+REM python -m nuitka src\updater\update.py --onefile
--- make_executable.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-subprocess.call(['venv\Scripts\pyinstaller.exe', '--onefile', 'src/updater/update.py'])
-subprocess.call(['python', '-m', 'nuitka', 'src\updater\update.py', '--onefile'])
